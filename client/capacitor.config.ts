3c7be14afe4908bf43dbc05c20a447ccc5d4528e
pmenezes08 <paulo.miguel.menezes@gmail.com>
2025-11-19 07:31:29 +0000

    chore: setup capacitor for ios

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.links.app',
+  appName: 'Links',
+  webDir: 'dist'
+};
+
+export default config;
